4d13d1f90e2175ca45681e33c997f998af12bb9d
Arian Behrami <arian.behrami@tu-dortmund.de>
2024-02-29 13:32:39 +0100

    git push

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,12 @@
 import view.GameApplication
 
+/**
+ * Main function to start the "Shift Poker" game application.
+ */
 fun main() {
+    // Instantiate and show the game application.
     GameApplication().show()
+
+    // Print a message indicating that the Shift Poker game has ended.
     println("Shift Poker game ended. Goodbye")
 }
--- src/main/kotlin/view/GameData.kt
+++ src/main/kotlin/view/GameData.kt
@@ -2,8 +2,16 @@ package view
 
 import entity.Card
 
+/**
+ * Object containing the data for the Shift Poker game.
+ */
 object GameData {
+    /** The number of rounds in the game. */
     var rounds: Int = 2
-    var playerNames: MutableList <String> = mutableListOf("Player 1", "Player 2")
-    var middleCards: MutableList <Card> = mutableListOf()
+
+    /** The names of the players participating in the game. */
+    var playerNames: MutableList<String> = mutableListOf("Player 1", "Player 2")
+
+    /** The middle cards available in the game. */
+    var middleCards: MutableList<Card> = mutableListOf()
 }
