1cc2329af20ba2c1a82db2b0025a5759bd4f5faa
Arian Behrami <arian.behrami@tu-dortmund.de>
2024-07-25 12:31:00 +0200

    git tag -a v1.0 -m "Release version 1.0"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,9 @@
-//import server.startServer
-//import client.startClient
 import view.GameApplication
-//import kotlinx.coroutines.*
 
 /**
  * Main Function
  */
 fun main() {
-    // Start the server in a separate thread
-//    Thread {
-//        startServer()
-//    }.start()
-//
-//    // Wait a few seconds to ensure the server is up and running
-//    Thread.sleep(2000)
-//
-//    // Start the client in a coroutine
-//    runBlocking {
-//        launch {
-//            startClient()
-//        }
-//    }
-
     // Instantiate and show the game application.
     GameApplication().show()
 
